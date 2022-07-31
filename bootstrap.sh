7a5520d658bb388d6ea1c1e5dcd89f4cd35b6bce
Michael Fisher <fisher@techallies.org>
2022-07-31 18:32:46 -0400

    feat: update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,16 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
+
+cat<<EOF > ~/.config/chezmoi/chezmoi.toml
+[data]
+email = "fisher@techallies.org"
+python.version = "3.9.1"
+sshAgentContainer.location = "2BUA8C4S2C.com.1password"
+[git]
+autoCommit = true
+autoPush = true
+[editor]
+command = "vim"
+EOF
 
 GITHUB_USERNAME=michaelefisher
-$ sh -c "$(curl -fsLS https://chezmoi.io/get)" -- init --apply $GITHUB_USERNAME
+sh -c "$(curl -fsLS https://chezmoi.io/get)" -- init --one-shot -k --apply $GITHUB_USERNAME
