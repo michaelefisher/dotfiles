e8374b8bbd767abc6bafb19ef40b73a5b94cdb79
Michael Fisher <fisher@techallies.org>
2022-07-31 20:45:24 -0400

    feat: move to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -46,5 +46,10 @@ EOF
 GITHUB_USERNAME=michaelefisher
 sh -c "$(curl -fsLS https://chezmoi.io/get)" -- init --one-shot -k --apply $GITHUB_USERNAME
 
+# Install gcloud cli
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-395.0.0-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-395.0.0-linux-x86.tar.gz
+./google-cloud-sdk/install.sh -q --screen-reader false
+
 export CLOUDSDK_CONFIG=$HOME/.gcloud
 
