3947327c9ddf018795695ad3eabf72105ee152a6
Michael Fisher <fisher@techallies.org>
2022-07-31 20:41:25 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,36 @@
 #!/usr/bin/env bash
 
+# This script only runs in the Google Cloud Shell environment
+
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+ sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
+ sudo tee /etc/apt/sources.list.d/1password.list
+
+mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
+ sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+ sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+
+apt-get update
+apt-get -y install \
+  gcc \
+  cmake \
+  python3.9-dev \
+  python3-pip \
+  libssl-dev \
+  make \
+  libutempter-dev \
+  zlib1g-dev \
+  1password-cli \
+  zsh
+
+# Download and install Starship
+sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes
+
 cat<<EOF > ~/.config/chezmoi/chezmoi.toml
 [data]
 email = "fisher@techallies.org"
@@ -14,3 +45,6 @@ EOF
 
 GITHUB_USERNAME=michaelefisher
 sh -c "$(curl -fsLS https://chezmoi.io/get)" -- init --one-shot -k --apply $GITHUB_USERNAME
+
+export CLOUDSDK_CONFIG=$HOME/.gcloud
+
