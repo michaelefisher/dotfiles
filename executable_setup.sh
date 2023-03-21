932eb802171ae50444ec5107fe2631189465795f
Michael Fisher <fishermichaele@gmail.com>
2023-03-21 10:36:56 -0400

    Update setup.sh

--- executable_setup.sh
+++ executable_setup.sh
@@ -19,14 +19,6 @@ rm -rf $CHEZMOI_DIR
 sh -c "$(curl -fsLS https://chezmoi.io/get)"
 $HOME/bin/chezmoi init --apply https://github.com/$GITHUB_USERNAME/dotfiles.git -k --config $DOTFILES_PATH/chezmoi.toml
 
-# # Install gcloud cli
-# wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-395.0.0-linux-x86_64.tar.gz
-# tar -xvf google-cloud-cli*
-# ./google-cloud-sdk/install.sh -q --screen-reader false
-
-# export CLOUDSDK_CONFIG=$HOME/.gcloud
-# export PATH=$HOME/google-cloud-sdk/bin:$HOME/bin:$PATH
-
 end=`date +%s`
 runtime=$((end-start))
 
