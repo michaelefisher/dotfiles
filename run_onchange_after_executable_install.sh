6ef4718e1ffed6ac44a631db5e42c49992964eaa
Michael Fisher <fishermichaele@gmail.com>
2024-03-25 14:51:24 -0400

    Update Brewfile Change attributes of executable_install.sh

--- run_onchange_after_executable_install.sh
+++ run_onchange_after_executable_install.sh
@@ -5,6 +5,6 @@ if [[ $(which shc | echo $?) == 0 ]]; then
 #		printf '%s' "No setup.sh file found in $HOME, run chezmoi apply."
 #		exit 1;
 #	fi
-	/opt/homebrew/bin/shc -o setup -f $HOME/setup.sh
+	shc -o setup -f $HOME/setup.sh
 fi
 
