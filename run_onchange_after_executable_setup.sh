583f871f7201689c5d50d24c72f8d5444293665b
Michael Fisher <fishermichaele@gmail.com>
2024-03-25 14:39:27 -0400

    Update executable_setup.sh

--- run_onchange_after_executable_setup.sh
+++ run_onchange_after_executable_setup.sh
@@ -5,6 +5,6 @@ if [[ $(which shc | echo $?) == 0 ]]; then
 #		printf '%s' "No setup.sh file found in $HOME, run chezmoi apply."
 #		exit 1;
 #	fi
-	shc -o setup -f $HOME/setup.sh
+	/opt/homebrew/bin/shc -o setup -f $HOME/setup.sh
 fi
 
